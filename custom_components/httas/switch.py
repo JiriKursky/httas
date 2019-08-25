84fbe427e6407dfc856b56b18b822d89bd1c57e6
JiriKursky <jiri.kursky@gmail.com>
2019-08-25 12:24:32 +0200

    Add files via upload

--- custom_components/httas/switch.py
+++ custom_components/httas/switch.py
@@ -7,7 +7,7 @@ import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.switch import (SwitchDevice, PLATFORM_SCHEMA)
 from homeassistant.const import (CONF_PASSWORD, CONF_USERNAME, CONF_SWITCHES, CONF_FRIENDLY_NAME,
-     CONF_IP_ADDRESS, CONF_SCAN_INTERVAL, DEVICE_CLASS_POWER)
+     CONF_IP_ADDRESS, CONF_SCAN_INTERVAL, CONF_TYPE, DEVICE_CLASS_POWER)
 from homeassistant.helpers.event import async_call_later
 from inspect import currentframe, getframeinfo
 
@@ -16,11 +16,23 @@ ENTITY_ID_FORMAT = 'switch.{}'
 
 CONF_FOLLOW_DEVICE = 'follow_device'
 CONF_NOTIFICATION = 'notification'
-CMND_POWER = 'POWER'                # Get info if is ON/OFF
-CMND_POWER_ON = 'Power%20On'        # Comnmand to turn on
-CMND_POWER_OFF = 'Power%20Off'      # Comnmand to turn off
-MAX_LOST = 5                        # Can be lost in commincation
-R_POWER = 'POWER'                   # Return json
+CMND_POWER = 'POWER'                    # Get info if is ON/OFF
+CMND_POWER_1 = 'POWER1'                 # Get info if is ON/OFF
+CMND_POWER_2 = 'POWER2'                 # Get info if is ON/OFF
+CMND_POWER_ON = 'Power%20On'            # Comnmand to turn on
+CMND_POWER_OFF = 'Power%20Off'          # Comnmand to turn off
+CMND_POWER_1_ON = 'Power1%20On'          # Comnmand to turn on
+CMND_POWER_1_OFF = 'Power1%20Off'        # Comnmand to turn off
+CMND_POWER_2_ON = 'Power2%20On'          # Comnmand to turn on
+CMND_POWER_2_OFF = 'Power2%20Off'        # Comnmand to turn off
+MAX_LOST = 5                            # Can be lost in commincation
+R_POWER = 'POWER'                       # Return json
+R_POWER_1 = 'POWER1'                    # Return json
+R_POWER_2 = 'POWER2'                    # Return json
+
+TYPE_SINGLE = 'single'
+TYPE_DUAL_1 = 'dual_1'
+TYPE_DUAL_2 = 'dual_2'
 
 _LOGGER = logging.getLogger(__name__)
 HTTP_TIMEOUT = 5
@@ -31,6 +43,7 @@ SWITCH_SCHEMA = vol.Schema({
     vol.Optional(CONF_FOLLOW_DEVICE, default = True): cv.boolean,
     vol.Optional(CONF_FRIENDLY_NAME): cv.string,
     vol.Optional(CONF_NOTIFICATION, default = True): cv.boolean,
+    vol.Optional(CONF_TYPE, default = TYPE_SINGLE): cv.string,
     vol.Optional(CONF_SCAN_INTERVAL, default = 30): vol.All(cv.positive_int, vol.Range(min=3, max=59))
 })
 
@@ -80,6 +93,22 @@ class Sonoff(SwitchDevice):
         self._lost = 0
         self._lost_informed = False # info about losing
         self._info_state_ok = True  # info that everything is ok
+        device_type = pars.get(CONF_TYPE)
+        self._power_on = CMND_POWER_ON
+        self._power_off = CMND_POWER_OFF
+        self._power = CMND_POWER
+        self._rpower = R_POWER
+
+        if device_type == TYPE_DUAL_1:
+            self._power_on = CMND_POWER_1_ON
+            self._power_off = CMND_POWER_1_OFF
+            self._power = CMND_POWER_1
+            self._rpower = R_POWER_1
+        elif device_type == TYPE_DUAL_2:
+            self._power_on = CMND_POWER_2_ON
+            self._power_off = CMND_POWER_2_OFF
+            self._power = CMND_POWER_2
+            self._rpower = R_POWER_2        
 
     def _debug(self, s):
         cf = currentframe()
@@ -121,7 +150,7 @@ class Sonoff(SwitchDevice):
         if value is None:
             return False
         else:
-            self._debug("returned with success power: {}".format(value[R_POWER]))
+            self._debug("returned with success power: {}".format(value[self._rpower]))
             self._lost = 0
             if not self._info_state_ok:
                 if self._notification:
@@ -130,8 +159,8 @@ class Sonoff(SwitchDevice):
                         title=DOMAIN) 
                 self._info_state_ok = True                           
             if self._follow_device:
-                self._is_on = value[R_POWER] == 'ON'                  
-            elif self._is_on != value[R_POWER]: 
+                self._is_on = value[self._rpower] == 'ON'                  
+            elif self._is_on != value[self._rpower]: 
                 if self._is_on :
                     self.turn_on()
                 else:
@@ -143,7 +172,7 @@ class Sonoff(SwitchDevice):
     async def _do_update(self):
         """ Returning current state of sensor """
         self._debug("do update")
-        if await self._send(CMND_POWER):
+        if await self._send(self._power):
             self._debug("scan interval: {}".format(self._scan_interval))
             async_call_later(self.hass, self._scan_interval, self._do_update())        
         else:
@@ -184,13 +213,13 @@ class Sonoff(SwitchDevice):
         """Turn the switch on."""
         self._is_on = True
         self.async_schedule_update_ha_state()
-        self._send_cmnd(CMND_POWER_ON)        
+        self._send_cmnd(self._power_on)        
 
     def turn_off(self, **kwargs):
         """Turn the switch off."""
         self._is_on = False
         self.async_schedule_update_ha_state()
-        self._send_cmnd(CMND_POWER_OFF)        
+        self._send_cmnd(self._power_off)        
         
     @property
     def device_class(self):
